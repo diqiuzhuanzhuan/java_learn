3db084e6e71fb3ccbce462ac4d0c31f6538e18d7
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2019-05-27 00:14:49 +0800

    add exception

--- 03_sqrt/src/main/java/diqiuzhuanzhuan/App.java
+++ 03_sqrt/src/main/java/diqiuzhuanzhuan/App.java
@@ -1,6 +1,8 @@
 package diqiuzhuanzhuan;
 import org.apache.commons.math3.analysis.function.Sqrt;
 
+import java.lang.reflect.Array;
+
 /**
  * Hello world!
  *
@@ -9,10 +11,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "sqrt of 100 is " + sqrt(100));
+        System.out.println( "sqrt of 100 is " + sqrt(100) );
+        out_of_index();
     }
 
     public static double sqrt(double num) {
         return new Sqrt().value(num);
+    }
+
+    public static int out_of_index() {
+        int[] numbers = {1, 2, 3, 4};
+        try {
+            System.out.println(numbers[8]);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("the third element is " + numbers[2]);
+        }
+        return 0;
     }
 }
