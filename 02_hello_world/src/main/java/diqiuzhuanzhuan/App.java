28b65c7c75cfac968523ca84ed9e4d9a28732a11
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2019-05-26 16:13:02 +0800

    增加测试用例

--- 02_hello_world/src/main/java/diqiuzhuanzhuan/App.java
+++ 02_hello_world/src/main/java/diqiuzhuanzhuan/App.java
@@ -10,4 +10,7 @@ public class App
         System.out.println( "Hello World!" );
     }
 
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
--- 02_hello_world/src/test/java/diqiuzhuanzhuan/AppTest.java
+++ 02_hello_world/src/test/java/diqiuzhuanzhuan/AppTest.java
@@ -1,5 +1,6 @@
 package diqiuzhuanzhuan;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,10 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void test_add() {
+        assertEquals(3, new App().add(1, 2));
     }
 }
